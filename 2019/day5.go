e9e03e03baab5f8b5bbc3739d0c395de02406aa4
Alasdair Sinclair <alasdairsinc@gmail.com>
2019-12-18 19:39:36 +0000

    2019: Day 5

---
+++ 2019/day5.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s filename\n", os.Args[0])
+		os.Exit(0)
+	}
+	program := getProgram(os.Args[1])
+	run(program)
+}
+
+func decodeOpCode(value int) (int, []int) {
+	var ret []int
+	var opCode = value % 100
+
+	value = value / 100
+	for i:=0 ; i < 4; i++ {
+		ret = append(ret, value % 10)
+		value = value / 10
+	}
+
+	return opCode, ret
+}
+
+func read(memory []int, parameterMode int, parameter int) int {
+	if parameterMode == 0 {
+		return memory[parameter]
+	} else if parameterMode == 1 {
+		return parameter
+	} else {
+		log.Fatal("Unknown parameterMode")
+	}
+	return 0
+}
+
+func run(memory []int) []int {
+	ip := 0
+	var opcode int
+	var parameterModes []int
+
+	program := make([]int, len(memory))
+
+	copy(program, memory)
+
+	for program[ip] != 99 {
+		opcode, parameterModes = decodeOpCode(program[ip])
+
+		switch opcode {
+		case 1: //add
+			res := read(program, parameterModes[0], program[ip + 1])
+			res = res + read(program, parameterModes[1], program[ip + 2])
+			program[program[ip + 3]] = res
+			ip = ip + 4
+		case 2: //multiply
+			res := read(program, parameterModes[0], program[ip + 1])
+			res = res * read(program, parameterModes[1], program[ip + 2])
+			program[program[ip + 3]] = res
+			ip = ip + 4
+		case 3: //read int
+			var input int
+			fmt.Scanln(&input)
+			program[program[ip+1]] = input
+			ip = ip + 2
+		case 4: //print int
+			val := read(program, parameterModes[0], program[ip + 1])
+			fmt.Println(val)
+			ip = ip + 2
+		case 5: //jump-if-true
+			val := read(program, parameterModes[0], program[ip + 1])
+			if val != 0 {
+				ip = read(program, parameterModes[1], program[ip + 2])
+			} else {
+				ip = ip + 3
+			}
+		case 6: //jump-if-false
+			val := read(program, parameterModes[0], program[ip + 1])
+			if val == 0 {
+				ip = read(program, parameterModes[1], program[ip + 2])
+			} else {
+				ip = ip + 3
+			}
+		case 7: //less than
+			val1 := read(program, parameterModes[0], program[ip + 1])
+			val2 := read(program, parameterModes[1], program[ip + 2])
+			if val1 < val2 {
+				program[program[ip + 3]] = 1
+			} else {
+				program[program[ip + 3]] = 0
+			}
+
+			ip = ip + 4
+		case 8: //equals
+			val1 := read(program, parameterModes[0], program[ip + 1])
+			val2 := read(program, parameterModes[1], program[ip + 2])
+			if val1 == val2 {
+				program[program[ip + 3]] = 1
+			} else {
+				program[program[ip + 3]] = 0
+			}
+
+			ip = ip + 4
+		default:
+			log.Fatalf("Unknown opcode %d\n", opcode)
+		}
+	}
+	return program
+}
+
+func getProgram(filename string) []int {
+	var input string
+	var program []int
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	onComma := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		for i := 0; i < len(data); i++ {
+			if data[i] == ',' {
+				return i + 1, data[:i], nil
+			}
+		}
+		if !atEOF {
+			return 0, nil, nil
+		}
+		// There is one final token to be delivered, which may be the empty string.
+		// Returning bufio.ErrFinalToken here tells Scan there are no more tokens after this
+		// but does not trigger an error to be returned from Scan itself.
+		return 0, data, bufio.ErrFinalToken
+	}
+
+	scanner.Split(onComma)
+
+	for	scanner.Scan() {
+		input = strings.TrimRight(scanner.Text(), "\n")
+
+		i, err := strconv.Atoi(input)
+		if err == nil {
+			program = append(program, i)
+		}
+	}
+
+	return program
+}
