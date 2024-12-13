7133d21a472de327827e8d4b78ce5eed04bf2ab1
Alasdair Sinclair <alasdairsinc@gmail.com>
2024-12-13 20:25:09 +0000

    2024: Day 5, Part 2

--- 2024/day05.py
+++ 2024/day05.py
@@ -1,9 +1,8 @@
-def main():
+def get_data(filename):
     rules = []
     updates = []
-    part1 = 0
 
-    with open('day05.input', 'r') as f:
+    with open(filename, 'r') as f:
         lines = [line.strip() for line in f.readlines()]
 
     for line in lines:
@@ -13,6 +12,33 @@ def main():
         elif ',' in line:
             nums = [int(n) for n in line.split(sep=',')]
             updates.append(nums)
+
+    return (rules, updates)
+
+def sort_update(update, rules):
+    swaps = True
+    while swaps:
+        swaps = False
+        for rule in rules:
+            if rule[0] in update and rule[1] in update:
+                i0 = update.index(rule[0])
+                i1 = update.index(rule[1])
+
+                if i0 < i1:
+                    pass
+                else:
+                    #print(f"pre swap update: {update}, swap {i0}, {i1}")
+                    swaps = True
+                    tmp = update[i0]
+                    update[i0] = update[i1]
+                    update[i1] = tmp
+                    #print(f"post swap update: {update}, swap {i0}, {i1}")
+
+def main():
+    part1 = 0
+    part2 = 0
+    incorrect_updates = []
+    (rules, updates) = get_data('day05.input')
 
     for update in updates:
         ok = True
@@ -25,8 +51,19 @@ def main():
                 part1 += update[int((len(update) + 1) / 2) - 1]
             else:
                 print("OH KNOW")
+        else:
+            incorrect_updates.append(update)
+
+    for update in incorrect_updates:
+        sort_update(update, rules)
+
+        if len(update) % 2 == 1:
+            part2 += update[int((len(update) + 1) / 2) - 1]
+        else:
+            print("OH KNOW")
 
     print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
 
 if __name__ == "__main__":
     main()
